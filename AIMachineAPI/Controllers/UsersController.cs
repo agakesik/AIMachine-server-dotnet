2bef555e81e572cc2ac72608bc20e3ef42e86fdd
agakesik <aga.k396@gmail.com>
2022-01-24 09:50:35 +0100

    Add User Model and Users Controller

---
+++ AIMachineAPI/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using AIMachineAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace AIMachineAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        public static List<User> users = new List<User>
+            {
+                new User { Id = 1, Username = "test", Password = "test"}
+            };
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("self")]
+        public async Task<IActionResult> GetSelf()
+        {
+            return Ok("self");
+        }
+
+        [HttpGet("login")]
+        public async Task<IActionResult> Login()
+        {
+            return Ok("login");
+        }
+
+        [HttpGet("logout")]
+        public async Task<IActionResult> Logout()
+        {
+            return Ok("logout");
+        }
+        [HttpPost("create")]
+        public async Task<ActionResult<List<User>>> AddUser(User user)
+        {
+            users.Add(user);
+            return Ok(user);
+        }
+    }
+}
